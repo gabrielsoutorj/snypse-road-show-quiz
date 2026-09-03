22eb9cf5737a780af84dec6b25d176d490498bcf
gabrielsoutorj <96085094+gabrielsoutorj@users.noreply.github.com>
2026-09-02 18:26:14 -0700

    Start background music across the quiz

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { AppErrorBoundary } from './components/AppErrorBoundary'
+import { BackgroundMusic } from './components/BackgroundMusic'
 import { router } from './router'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppErrorBoundary>
+      <BackgroundMusic />
       <RouterProvider router={router} />
     </AppErrorBoundary>
   </StrictMode>,
